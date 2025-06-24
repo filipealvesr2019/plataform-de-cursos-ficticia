8468f18a15dbcc030934b35d9910ea68a78cbb48
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2025-06-24 15:03:44 -0300

    Projeto: Plataforma de Cursos Online (como Udemy simplificado)

--- contexts/AuthContext.js
+++ contexts/AuthContext.js
@@ -19,11 +19,20 @@ export function AuthProvider({ children }){
         const data = await res.json();
 
         if(res.ok){
+            localStorage.setItem("token", data.token);
+            setUser({ email });
 
         } else {
+            throw new Error(data.error || "Erro ao logar")
             
         }
         
+    }
+
+    const register = async (name, email, password) => {
+        const res = await fetch("http://localhost:5000/api/auth/register", {
+            
+        })
     }
 
 }
